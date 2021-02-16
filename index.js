391bb66eb0f07b7f125f48f2a9cd09eb0acf92dd
fentyhall <fentymhall@gmail.com>
2021-02-15 17:08:28 -0800

    finish lab

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Your code here
+function saturdayFun(string = "roller-skate") {
+    return `This Saturday, I want to ${string}!`
+}
+
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+function wrapAdjective(wrapper = "*") {
+    return function(string = "special") {
+        return `You are ${wrapper}${string}${wrapper}!`
+    }
+}
+
+const Calculator = {
+    add: function(a,b) {
+        return a + b;
+    },
+    subtract: function(a,b) {
+        return a - b
+    },
+    multiply: function(a,b) {
+        return a * b
+    },
+    divide: function(a,b) {
+        return a / b;
+    }
+}
+
+const actionApplyer = function(start, arr) {
+    let int = start;
+    for (let fn of arr) {
+        int = fn(int)
+    }
+    return int;
+}
